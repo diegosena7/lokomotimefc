f7cfbdbc6a3af7b06970e32e14f6ec04b045294d
diegosena7 <diego.tjdosantos@gmail.com>
2022-06-27 23:25:52 -0300

    ajuste nos conflitos de mapeamento e lombok

--- src/main/java/br/com/dsena7/lokomotimefc/model/mapper/JogadorMapper.java
+++ src/main/java/br/com/dsena7/lokomotimefc/model/mapper/JogadorMapper.java
@@ -3,9 +3,11 @@ package br.com.dsena7.lokomotimefc.model.mapper;
 import br.com.dsena7.lokomotimefc.model.dto.JogadorDTO;
 import br.com.dsena7.lokomotimefc.model.entity.Jogador;
 import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 @Mapper(componentModel = "spring")
 public interface JogadorMapper {
 
